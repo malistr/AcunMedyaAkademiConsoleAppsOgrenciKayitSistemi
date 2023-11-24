4d9a0a22f8814da6bad7f74f8de2f1c2025e1032
Mehmet Ali Satar <mehmetalisatarr@gmail.com>
2023-11-24 19:25:51 +0300

    örnek amaclı commit

--- Siniflar/Program.cs
+++ Siniflar/Program.cs
@@ -3,7 +3,7 @@
     internal class Program
     {
 
-      
+
         static void Main(string[] args)
         {
             Arsba araba = new Arsba();
@@ -13,11 +13,11 @@
             araba.TekerlekSayisi = 2;
             araba.Renk = "kirmizi";
             araba.ModelYili = 3;
-            
+
             List<Arsba> list = new List<Arsba>();
             list.Add(araba);
 
-            Otobus otobus = new Otobus();   
+            Otobus otobus = new Otobus();
             otobus.Id = 2;
             otobus.Renk = "kırmı<ı";
             otobus.Marka = "öppe";
@@ -26,16 +26,52 @@
             otobus.YolcuADeti = 1;
 
 
-            Arsba araba1 = new Arsba { 
+            Arsba araba1 = new Arsba {
                 Id = 1,
                 Marka = "bobo",
                 ModelYili = 111,
-                Name = "nsms", 
-                Renk = "www", 
-                TekerlekSayisi = 11 };
+                Name = "nsms",
+                Renk = "www",
+                TekerlekSayisi = 11
+            };
             list.Add(araba1);
+
+
+            Arsba araba2 = new Arsba {
+                Id = 2,
+                TekerlekSayisi = 1,
+                Renk = "sas",
+                Name = "MAA ms",
+                ModelYili = 111,
+                Marka = "mn sasmm",
+                YolcuADeti = 1
+            };
+
+            list.Add(araba2);
+
+            Otobus otobus2 = new Otobus {
+                Id = 3,
+                YolcuADeti = 12,
+                Marka = "sad",
+                ModelYili = 111,
+                Name = "dasdsd",
+                Renk = "<zszdhbs",
+                TekerlekSayisi = 1,
+                AyaktaYolcuAdeti = 1,
+                EngelliRampasi = false
+
+                // ornek yapmak ıcın
+
+
+
+            };
+
+            Otobus otobus3 = new Otobus
+            {
+                Id = 2,
+        };
         }
 
     }
-  
+
 }
